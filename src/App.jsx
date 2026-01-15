22808d07da8f7ada1f9105c87ac60b14833a7e71
Ismael <mihaja.ismael@gmail.com>
2026-01-15 10:56:16 +0300

    Initializing project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-blue-500 text-4xl font-bold">
+        Tailwind fonctionne ✅
+      </h1>
+    </div>
+  );
+}
